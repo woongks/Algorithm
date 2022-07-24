0cb32087503b207a97084dad7fa34d5edf207941
ksw <tlsdnddl12@naver.com>
2022-07-24 13:07:09 +0900

    [Add] frequencyCounter.js

---
+++ [udemy] JavaScript Algorithm/frequencyCounter.js
@@ -0,0 +1,28 @@
+```
+write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same
+```;
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1; // add 1 if it exists else initialize it
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false; // ~ in object compares keys
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(same([1, 2, 1], [4, 1, 1]));
