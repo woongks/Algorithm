10ec6f792157b1900f2406e5bfab9be616e7d47e
ksw <tlsdnddl12@naver.com>
2022-07-27 01:48:56 +0900

    [Add] isSubsequence.js

---
+++ [udemy] JavaScript Algorithm/multiplePointers/isSubsequence.js
@@ -0,0 +1,22 @@
+```
+write a function called isSubsequence which takes in two strings and checks whethers the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+```;
+
+function isSubsequence(str1, str2) {
+  let pointer = 0;
+  let pointer2 = 0;
+  while (pointer2 < str2.length) {
+    if (str1[pointer] == str2[pointer2]) {
+      pointer++;
+      pointer2++;
+    } else {
+      pointer2++;
+    }
+  }
+  if (pointer == str1.length) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isSubsequence("hello", "hello world"));
